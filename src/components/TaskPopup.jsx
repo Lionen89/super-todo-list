83ed41bb7821d1f66d1f627d9ddd60283c911709
Lionen89 <Lionen89@yandex.ru>
2023-01-26 14:17:03 +0700

    feat/added TaskPopup component

---
+++ src/components/TaskPopup.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import Popup from './Popup';
+
+function TaskPopup({ isOpen, onClose, onSubmit }) {
+  const [name, setName] = useState('');
+  const [description, setdDescription] = useState('');
+  const handleClosePopup = () => {
+    onClose();
+    setName('');
+    setdDescription('');
+  };
+  function handleSubmit(e) {
+    e.preventDefault();
+    onSubmit(name, description);
+    handleClosePopup();
+  }
+
+  return (
+    <Popup title="Add Tsk" isOpen={isOpen} onClose={handleClosePopup} onSubmit={handleSubmit}>
+      <label className="popup__form-field">
+        <input
+          type="text"
+          className="popup__input popup__text"
+          id="name-input"
+          name="name"
+          required
+          minLength="1"
+          maxLength="200"
+          value={name}
+          placeholder="Task name"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+      </label>
+      <label className="popup__form-field">
+        <input
+          type="text"
+          className="popup__input popup__text"
+          id="description-input"
+          name="description"
+          minLength="1"
+          maxLength="1000"
+          placeholder="Description"
+          value={description}
+          onChange={(e) => {
+            setdDescription(e.target.value);
+          }}
+        />
+      </label>
+    </Popup>
+  );
+}
+export default TaskPopup;
